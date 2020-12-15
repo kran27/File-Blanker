55d7b8acd087bae495e1aec5113440b80796a0f6
Kranberri <56488228+kran27@users.noreply.github.com>
2020-12-14 18:57:16 -0800

    Improved code + added subdirectory support
    i figured out a better way of scanning for files.

--- File Blanker/Form1.vb
+++ File Blanker/Form1.vb
@@ -4,12 +4,8 @@
         TextBox1.Text = FolderBrowserDialog1.SelectedPath
     End Sub
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Dim dirinfo As New IO.DirectoryInfo(TextBox1.Text)
-        Dim getfiles As IO.FileInfo() = dirinfo.GetFiles()
-        Dim file As IO.FileInfo
-
-        For Each file In getfiles
-            IO.File.WriteAllBytes(file.FullName, My.Resources.blankfile)
+        For Each file As String In My.Computer.FileSystem.GetFiles(TextBox1.Text, FileIO.SearchOption.SearchAllSubDirectories, "*")
+            IO.File.WriteAllBytes(file, My.Resources.blankfile)
         Next
         MsgBox("Done Writing!", MsgBoxStyle.Information Or MsgBoxStyle.OkOnly, "Process Complete")
     End Sub
