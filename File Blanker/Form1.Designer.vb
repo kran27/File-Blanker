870953176898ce160bee0813339fac073685803f
Kranberri <56488228+kran27@users.noreply.github.com>
2022-02-26 17:28:54 -0800

    Use DarkUI, Update to .NET 5.0, better folder browser

--- File Blanker/Form1.Designer.vb
+++ File Blanker/Form1.Designer.vb
@@ -1,6 +1,6 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class Form1
-    Inherits System.Windows.Forms.Form
+    Inherits DarkUI2.Forms.DarkForm
 
     'Form overrides dispose to clean up the component list.
     <System.Diagnostics.DebuggerNonUserCode()> _
@@ -23,58 +23,69 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.TextBox1 = New DarkUI2.Controls.DarkTextBox()
+        Me.Button2 = New DarkUI2.Controls.DarkButton()
+        Me.Button1 = New DarkUI2.Controls.DarkButton()
+        Me.Label1 = New DarkUI2.Controls.DarkLabel()
         Me.SuspendLayout()
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(69, Byte), Integer), CType(CType(73, Byte), Integer), CType(CType(74, Byte), Integer))
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextBox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(220, Byte), Integer), CType(CType(220, Byte), Integer), CType(CType(220, Byte), Integer))
+        Me.TextBox1.Location = New System.Drawing.Point(13, 12)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(213, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(248, 23)
         Me.TextBox1.TabIndex = 1
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(11, 37)
+        Me.Button2.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button2.Location = New System.Drawing.Point(13, 41)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(319, 23)
+        Me.Button2.Padding = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.Button2.Size = New System.Drawing.Size(382, 23)
         Me.Button2.TabIndex = 2
         Me.Button2.Text = "Write Blank Data"
-        Me.Button2.UseVisualStyleBackColor = True
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(249, 11)
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.Location = New System.Drawing.Point(297, 12)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(81, 22)
+        Me.Button1.Padding = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.Button1.Size = New System.Drawing.Size(98, 23)
         Me.Button1.TabIndex = 3
         Me.Button1.Text = "Pick Directory"
-        Me.Button1.UseVisualStyleBackColor = True
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(229, 16)
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(269, 16)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(18, 13)
+        Me.Label1.Size = New System.Drawing.Size(20, 15)
         Me.Label1.TabIndex = 4
         Me.Label1.Text = "Or"
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(341, 71)
+        Me.ClientSize = New System.Drawing.Size(408, 76)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.TextBox1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "Form1"
@@ -84,9 +95,9 @@ Partial Class Form1
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Label1 As Label
-    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+
+    Friend WithEvents TextBox1 As DarkUI2.Controls.DarkTextBox
+    Friend WithEvents Button2 As DarkUI2.Controls.DarkButton
+    Friend WithEvents Button1 As DarkUI2.Controls.DarkButton
+    Friend WithEvents Label1 As DarkUI2.Controls.DarkLabel
 End Class
--- File Blanker/Form1.vb
+++ File Blanker/Form1.vb
@@ -1,12 +1,13 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        FolderBrowserDialog1.ShowDialog()
-        TextBox1.Text = FolderBrowserDialog1.SelectedPath
+        Dim FBD As FolderBrowserDialog = New FolderBrowserDialog()
+        FBD.ShowDialog()
+        TextBox1.Text = FBD.SelectedPath
     End Sub
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         For Each file As String In My.Computer.FileSystem.GetFiles(TextBox1.Text, FileIO.SearchOption.SearchAllSubDirectories, "*")
             IO.File.WriteAllBytes(file, My.Resources.blankfile)
         Next
-        MsgBox("Done Writing!", MsgBoxStyle.Information Or MsgBoxStyle.OkOnly, "Process Complete")
+        DarkUI2.Forms.DarkMessageBox.ShowInformation("Done Writing!", "Process Complete")
     End Sub
 End Class
